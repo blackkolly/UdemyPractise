2a8da4a0ab4ec1a9cfea7f0501e8000ac7a20ad4
blackkolly <komolafe.solomon@yahoo.com>
2022-05-14 11:11:09 +0100

    new object

--- src/com/company/practise/kingdom/Main.java
+++ src/com/company/practise/kingdom/Main.java
@@ -3,6 +3,7 @@ package com.company.practise.kingdom;
 public class Main {
     public static void main(String[] args){
         Animal reptile = new Animal("Dog","black","height");
+        Animal mammal = new Animal("fish","silver","centimeter");
         System.out.println(reptile.color +"" + reptile.name + " is nice with a lovely "+ reptile.height);
         Animal display = new Animal();
         display.displayAnimal();
