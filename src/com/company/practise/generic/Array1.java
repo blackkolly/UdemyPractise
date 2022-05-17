52d1b80d5cc2c01c209e1775b0c712a223aa361c
blackkolly <komolafe.solomon@yahoo.com>
2022-05-17 16:56:15 +0100

    arraylist

---
+++ src/com/company/practise/generic/Array1.java
@@ -0,0 +1,13 @@
+package com.company.practise.generic;
+
+import java.util.ArrayList;
+
+public class Array1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(0,"kola");
+        list.add(1,"Taiwo");
+        list.add(2,"Emeka");
+        System.out.println(list);
+    }
+}
